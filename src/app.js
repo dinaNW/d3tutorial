84b027be8327b142cef2708d2722427ad539e53d
Dina Malone <dinamalone@Dinas-MacBook-Pro.local>
2017-05-09 11:05:20 +0100

    data updated, stacked chart WIP

--- src/app.js
+++ src/app.js
@@ -1,9 +1,9 @@
 var dataset = [
-    {name: 'Yahoo', score: 96, age: 20},
-    {name: 'ESPN', score: 83, age: 30},
-    {name: 'MTV', score: 91, age: 10},
-    {name: 'NYTimes', score: 96, age: 15},
-    {name: 'Huffington Post', score: 88, age: 25}
+    {name: 'Yahoo', live: 20, native: 25, embedded: 55},
+    {name: 'ESPN', live: 83, native: 7, embedded: 10},
+    {name: 'MTV', live: 91, native: 5, embedded: 4},
+    {name: 'NYTimes', live: 30, native: 25, embedded: 45},
+    {name: 'Huffington Post', live: 50, native: 25, embedded: 15}
 ];
 var margin = {top: 20, right: 40, bottom: 40, left: 85};
 
@@ -15,12 +15,6 @@ var height = +svg.attr('height') - margin.top - margin.bottom;
 
 var g = svg.append('g')
     .attr('transform', 'translate(' + margin.left + ', ' + margin.top + ')');
-
-// g.append('rect')
-//     .attr('width', width)
-//     .attr('height', height)
-//     .style('fill', 'lightblue')
-//     .style('stroke', 'green');
 
 var yScale = d3.scaleBand()
     .domain(dataset.map(function (d) {
@@ -43,20 +37,10 @@ g.append('g')
     .attr('transform', 'translate(0, ' + height + ')')
     .call(xAxis);
 
-// var chart = g.data(scores)
-//     .enter()
-//     .append('g')
-//     .attr('transform', function (d, i) {
-//         return 'translate(0, ' + i * 33 + ')';
-//     });
-// chart.append('rect')
-//     .style('width', function (d) {
-//         return d.score
-//     })
-//     .attr('class', 'bar');
-//
-// chart.append('text')
-//     .attr('y', 20)
-//     .text(function (d) {
-//         return d.name;
-//     });
+var zScale = d3.scaleOrdinal()
+    .range(["#356f75", "#409ac2", "#b4c68f"])
+    .domain(Object.keys(dataset[0]).slice(1));
+
+var stack = d3.stack()
+    .offset(d3.stackOffsetExpand);
+
