9ef60af70a9697c72646f0993a3c225c6546c325
Dina Malone <dina.malone@newswhip.com>
2017-05-09 11:21:45 +0100

    stacked chart WIP

--- src/app.js
+++ src/app.js
@@ -5,6 +5,8 @@ var dataset = [
     {name: 'NYTimes', live: 30, native: 25, embedded: 45},
     {name: 'Huffington Post', live: 50, native: 25, embedded: 15}
 ];
+var colorMapping = Object.keys(dataset[0]).slice(1);
+
 var margin = {top: 20, right: 40, bottom: 40, left: 85};
 
 var svg = d3.select('svg').attr('width', 350).attr('height', 400);
@@ -16,31 +18,54 @@ var height = +svg.attr('height') - margin.top - margin.bottom;
 var g = svg.append('g')
     .attr('transform', 'translate(' + margin.left + ', ' + margin.top + ')');
 
-var yScale = d3.scaleBand()
+var y = d3.scaleBand()
     .domain(dataset.map(function (d) {
         return d.name;
     }))
     .range([height, 0]);
 
-var yAxis = d3.axisLeft(yScale);
+var yAxis = d3.axisLeft(y);
 
 g.append('g')
     .call(yAxis);
 
-var xScale = d3.scaleLinear()
+var x = d3.scaleLinear()
     .domain([0, 1])
     .range([0, width]);
 
-var xAxis = d3.axisBottom(xScale).ticks(5, '%');
+var xAxis = d3.axisBottom(x).ticks(5, '%');
 
 g.append('g')
     .attr('transform', 'translate(0, ' + height + ')')
     .call(xAxis);
 
-var zScale = d3.scaleOrdinal()
+var z = d3.scaleOrdinal()
     .range(["#356f75", "#409ac2", "#b4c68f"])
-    .domain(Object.keys(dataset[0]).slice(1));
+    .domain(colorMapping);
 
 var stack = d3.stack()
     .offset(d3.stackOffsetExpand);
 
+var bars = g.selectAll(".bar")
+    .data(stack.keys(colorMapping)(dataset))
+    .enter().append("g")
+    .attr("class", "bar")
+    .attr("fill", function(d) {
+        return z(d.key);
+    });
+
+bars.selectAll("rect")
+    .data(function(d) {
+        return d;
+    })
+    .enter().append("rect")
+    .attr("y", function(d) {
+        return y(d.data.State);
+    })
+    .attr("x", function(d) {
+        return x(d[1]);
+    })
+    .attr("width", function(d) {
+        return x(d[0]);
+    })
+    .attr("height", y.bandwidth());
